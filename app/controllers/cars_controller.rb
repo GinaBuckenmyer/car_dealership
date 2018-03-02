f196c2d8ff940f55f351f8ec69ab02b7445ef1c9
GinaBuckenmyer <gina.buckenmyer@yahoo.com>
2018-03-01 19:20:49 -0600

    2nd commit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < ApplicationController
   def index
-    @cars = ["1963 Chevrolet Corvair", "1975 Ford Pinto", "1989 Yugo GV", "1986 Suzuki Samurai", "1983 Ford Bronco II"]
+    @cars = Car.all
   end
 end
